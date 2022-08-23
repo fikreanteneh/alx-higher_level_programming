195bffc98ae89544ad46e1d7e6a39a84ec122785
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-08-23 23:46:46 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 char = 97
 for char in range(97, 123):
-    if char!= ord("e") and char!= ord("q")
-        print("{}".format(chr(char)), end='')
+    if char==101 and char == 113:
+        continue
+    print("{}".format(chr(char)), end='')

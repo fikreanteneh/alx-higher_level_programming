10563f91698d18357c9342c6bb2deb780357463c
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-08-24 00:49:29 +0300

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,7 +4,6 @@ def print_last_digit(number):
     if non_negative < 0:
         non_negative = -non_negative
         last_digit = non_negative % 10
-        last_digit = -last_digit
     else:
         last_digit = non_negative % 10
     print("{}".format(last_digit), end='')

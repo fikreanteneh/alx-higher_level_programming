2ea8093198b74910bb8b8d73a1a331e5a23b29e7
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-08-29 13:15:24 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 0:
+        print("")
     for i in matrix:
         k = len(i)
         for j in range(k):
             if j <  k - 1:
-                print(i[j], end = ' ')
+                print("{:d}".format(i[j]), end=" ")
                 continue
-            print(i[j])
+            print("{:d}".format(i[j])

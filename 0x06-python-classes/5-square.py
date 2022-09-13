db65b136353a74e0c560bbdaa2534fdb851e8f74
fikreanteneh <fikre.anteneh01@gmail.com>
2022-09-12 23:49:24 -0700

    class

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return (self.__size * self.__size)
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for row in range(self.__size):
+                for column in range(self.__size):
+                    print("#", end="")
+                print()

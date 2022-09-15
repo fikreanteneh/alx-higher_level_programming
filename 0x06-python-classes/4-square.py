bf3193ba2cba148ab7027cdb5a2d78bd41c5dbe1
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-09-15 05:25:13 +0300

    4square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -40,16 +40,4 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = 
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+        self.__size = value

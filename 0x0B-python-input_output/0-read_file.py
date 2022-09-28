8e913ba941bd235a15c71d1bd0c44b53008c881e
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-09-28 19:50:21 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,14 +3,15 @@
 Reads from a file and prints.
 """
 
+
 def read_file(filename=""):
     """Reads from filename and prints
     its contents to stdout.
 
     Args:
         - filename: name of the file
-        """
-    
+    """
+
     with open(filename) as f:
         read_text = f.read()
         print(read_text, end="")

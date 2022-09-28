19433154fa63c66a51d64176464a90778845fac0
fikreanteneh <105011492+fikreanteneh@users.noreply.github.com>
2022-09-28 19:44:22 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ reades a file"""
+
 def read_file(filename=""):
     """args"""
+    
     with open(filename) as f:
         read_text = f.read()
         print(read_text, end="")
